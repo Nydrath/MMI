2b0dfc64cf7c57ec35864604b2006268ececa7c1
Nydrath <mark.t.dewl@gmail.com>
2017-02-24 23:13:15 +0100

    Added initial studio files.

---
+++ app/src/main/java/mmo/ogp/mmi/MainDisplay.java
@@ -0,0 +1,13 @@
+package mmo.ogp.mmi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainDisplay extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_display);
+    }
+}

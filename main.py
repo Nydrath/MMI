de5ad8e873693761c19a4790a087cfd6bf7b9492
Nydrath <mark.t.dewl@gmail.com>
2017-02-25 16:02:07 +0100

    Added button.

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 import kivy
+from kivy.app import App
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.button import Button
 
-class HelloWorld(kivy.app.App):
+class UI(FloatLayout):
+    def __init__(self, **kwargs):
+        super(UI, self).__init__(**kwargs)
+        button = Button(text='Hello world', size_hint=(.6, .6), pos_hint={'x':.2, 'y':.2})
+        self.add_widget(button)
+
+class Launcher(App):
     def build(self):
-        return kivy.uix.Label(text='Hello world')
+        ui = UI()
+        return ui
 
 if __name__ == '__main__':
-    HelloWorld().run()
+    Launcher().run()

e4c26fbf2c5f43adcdc081da6b9cec33a9d19151
Nydrath <mark.t.dewl@gmail.com>
2017-02-25 17:16:34 +0100

    Misc aesthetic changes.

--- main.py
+++ main.py
@@ -6,13 +6,12 @@ from kivy.uix.button import Button
 class UI(FloatLayout):
     def __init__(self, **kwargs):
         super(UI, self).__init__(**kwargs)
-        button = Button(text='Hello world', size_hint=(.6, .6), pos_hint={'x':.2, 'y':.2})
+        button = Button(text='Do magic', size_hint=(.6, .6), pos_hint={'x':.2, 'y':.2})
         self.add_widget(button)
 
 class Launcher(App):
     def build(self):
-        ui = UI()
-        return ui
+        return UI()
 
 if __name__ == '__main__':
     Launcher().run()
